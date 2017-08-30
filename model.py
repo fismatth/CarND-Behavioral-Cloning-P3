a28791b620c8c67deb0ae2400fe6c6d2ef77943d
Matthias Fischer <ma.fischer91@gmail.com>
2017-08-30 22:56:04 +0200

    First working version

---
+++ model.py
@@ -0,0 +1,80 @@
+import os
+import csv
+import cv2
+import numpy as np
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D, MaxPooling2D
+from keras.layers.core import Dropout
+
+def get_img(source_path):
+    filename = source_path.split('/')[-1]
+    current_path = 'data/IMG/' + filename
+    return cv2.imread(current_path)
+
+steering_offset = 0.1
+
+samples = []
+with open('data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        samples.append(line)
+samples = samples[1:]
+
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                center_angle = float(batch_sample[3])
+                images.append(get_img(batch_sample[0]))
+                images.append(get_img(batch_sample[1]))
+                images.append(get_img(batch_sample[2]))
+                angles.append(center_angle)
+                angles.append(center_angle + abs(center_angle) * 3.0)
+                angles.append(center_angle - abs(center_angle) * 3.0)
+                
+            num_img = len(images)
+            for i in range(num_img):
+                images.append(cv2.flip(images[i], 1))
+                angles.append(-angles[i])
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield shuffle(X_train, y_train)
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(6,5,5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(600))
+model.add(Dropout(p=0.5))
+model.add(Dense(200))
+model.add(Dropout(p=0.5))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
+
+model.save('model.h5')
+print('Model saved')
